6a82f28b291488cb1e02f8ffc8a966ecdb8da621
Junior Magnus <juninhu.freerider@gmail.com>
2022-10-14 19:52:38 -0300

    Crate max limit 99999 of passwords

--- PasswordGen/script.js
+++ PasswordGen/script.js
@@ -4,6 +4,7 @@ function genpass() {
     document.getElementById("txtgenpass").value = ''
     var psw = document.getElementById('txtgenpass').value
     var res = document.getElementById('res')
+    res.innerHTML = ''
     
     //get info
     var pqtt = document.getElementById('pqtt').value
@@ -11,28 +12,34 @@ function genpass() {
     var pswlen = (document.getElementById('plen').value - 1)
     
     //create password
-    for (var i = 0; i <= pswlen; i++) {
-        var randomNumber = Math.floor(Math.random() * chars.length);
-        psw += chars.substring(randomNumber, randomNumber +1);
-       }
-
+    if (pqtt > 1) {
+        if (pqtt > 99999) {
+            pqtt = 99999
+            document.getElementById('pqtt').value = pqtt
+            alert(`Max passwords that can be created is ${pqtt}`)
+        }
+        pqtt--
+        for(var i = 0; i <= pqtt; i++) {
+            var psw1 = ''
+            for (var j = 0; j <= pswlen; j++) {
+                var randomNumber = Math.floor(Math.random() * chars.length);
+                psw1 += chars.substring(randomNumber, randomNumber +1);
+            }
+            //create an ordered list
+            let item = document.createElement('li')
+            item.innerText = psw1
+            res.appendChild(item)
+            }
+    } else {
+        for (var i = 0; i <= pswlen; i++) {
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            psw += chars.substring(randomNumber, randomNumber +1);
+           }
+    }
+    
     //copy to clipboard
     document.getElementById("txtgenpass").value = psw
     var copyText = document.getElementById("txtgenpass");
     copyText.select();
     document.execCommand("copy");
-
-    //for more than 1 password
-    if (pqtt > 1) {
-        var morepss = document.createElement('div')
-        res.appendChild(morepss)
-        alert(`creating ${pqtt} passwords!`)
-        for(var i = 0; i <= pqtt; i++) {
-            for (var j = 0; j <= pswlen; j++) {
-                var randomNumber = Math.floor(Math.random() * chars.length);
-                psw += chars.substring(randomNumber, randomNumber +1);
-               }
-            morepss.appendChild(psw)
-        }
-    }
 }
