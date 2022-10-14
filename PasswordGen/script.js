40ab4fbb27d8c01f55683a46efe1a60517588f6c
jrmagnus <juninhu.freerider@gmail.com>
2022-10-14 18:01:50 -0300

    Create more than one password, not finished

--- PasswordGen/script.js
+++ PasswordGen/script.js
@@ -1,7 +1,12 @@
-function genpass() {
-    //to Password Gerenation
+//to Password Gerenation
+function genpass() {    
+    //clear textbox
     document.getElementById("txtgenpass").value = ''
-    let psw = document.getElementById('txtgenpass').value
+    var psw = document.getElementById('txtgenpass').value
+    var res = document.getElementById('res')
+    
+    //get info
+    var pqtt = document.getElementById('pqtt').value
     var chars = "0123456789abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ";
     var pswlen = (document.getElementById('plen').value - 1)
     
@@ -11,15 +16,23 @@ function genpass() {
         psw += chars.substring(randomNumber, randomNumber +1);
        }
 
+    //copy to clipboard
     document.getElementById("txtgenpass").value = psw
     var copyText = document.getElementById("txtgenpass");
     copyText.select();
-    document.execCommand("copy"); 
+    document.execCommand("copy");
+
+    //for more than 1 password
+    if (pqtt > 1) {
+        var morepss = document.createElement('div')
+        res.appendChild(morepss)
+        alert(`creating ${pqtt} passwords!`)
+        for(var i = 0; i <= pqtt; i++) {
+            for (var j = 0; j <= pswlen; j++) {
+                var randomNumber = Math.floor(Math.random() * chars.length);
+                psw += chars.substring(randomNumber, randomNumber +1);
+               }
+            morepss.appendChild(psw)
+        }
+    }
 }
-/*
-function copy() {
-    var copyText = document.getElementById("txtgenpass");
-    copyText.select();
-    document.execCommand("copy");  
-  }
-  */
