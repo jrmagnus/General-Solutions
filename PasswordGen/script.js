60bf8965a2f74332cf506464edc68f1d46de7a6a
jrmagnus <juninhu.freerider@gmail.com>
2022-10-14 17:10:56 -0300

    First test with Password Generation

--- PasswordGen/script.js
+++ PasswordGen/script.js
@@ -1,3 +1,25 @@
 function genpass() {
     //to Password Gerenation
+    document.getElementById("txtgenpass").value = ''
+    let psw = document.getElementById('txtgenpass').value
+    var chars = "0123456789abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ";
+    var pswlen = (document.getElementById('plen').value - 1)
+    
+    //create password
+    for (var i = 0; i <= pswlen; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        psw += chars.substring(randomNumber, randomNumber +1);
+       }
+
+    document.getElementById("txtgenpass").value = psw
+    var copyText = document.getElementById("txtgenpass");
+    copyText.select();
+    document.execCommand("copy"); 
 }
+/*
+function copy() {
+    var copyText = document.getElementById("txtgenpass");
+    copyText.select();
+    document.execCommand("copy");  
+  }
+  */
