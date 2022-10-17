1909fda6a0a0935dab2247505583172de084cce0
Junior Magnus <juninhu.freerider@gmail.com>
2022-10-16 22:56:10 -0300

    Styles Cards

--- gs-script.js
+++ gs-script.js
@@ -1,15 +1,3 @@
-//card styles
-document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
-
 //to Password Gerenation
 function genpsw() {    
     //clear textbox
